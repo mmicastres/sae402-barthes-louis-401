0215d15b0ec0b7754c03a75270c1bb2495ac7d24
Lyrastico <louisbarthes1@gmail.com>
2023-04-10 12:00:12 +0200

    changes

--- sae-402/src/App.js
+++ sae-402/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import * as React from "react";
+import NavBar from "./components/NavBar"
+
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className='app'>
+        <BrowserRouter>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/profile"></Route>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </body>
   );
 }
 
