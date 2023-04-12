60957025c884948195bcb34a0a2f8b200d0940ab
louisbarthes1@gmail.com <louisbarthes1@gmail.com>
2023-04-13 01:05:28 +0200

    g

--- 402_mobile/App.js
+++ 402_mobile/App.js
@@ -1,36 +1,62 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-function NotificationsScreen() {
+function ProfileScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
     </View>
   );
 }
 
+function ProjectsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Projects!</Text>
+    </View>
+  );
+}
 
+const Tab = createBottomTabNavigator();
 
-const Drawer = createDrawerNavigator();
-
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'user';
+            } else if (route.name === 'Projects') {
+              iconName = 'folder';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Projects" component={ProjectsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
